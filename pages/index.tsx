2e5305145161b9ff9e5aa66ee2258a6b493888d1
katai5plate <katai5plate.sub@gmail.com>
2019-06-03 14:47:29 +0900

    add: material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,14 @@
-export default () => <div>Welcome to next.js!</div>
+import Button from '@material-ui/core/Button';
+import React from 'react';
+
+export default class C extends React.Component {
+  onClick() {
+    fetch("http://localhost:3332/user/1")
+      .then(res => console.log(res))
+  }
+  render() {
+    return <Button variant="contained" color="primary" onClick={this.onClick}>
+      Welcome to next.js!
+    </Button>
+  }
+}
