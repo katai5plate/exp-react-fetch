7d43a9beccf1950f3a375ad3fe6baf040fbcbee8
katai5plate <katai5plate.sub@gmail.com>
2019-06-05 03:28:33 +0900

    fix: components - Button - InputText - Label - InputForm

--- pages/components/molecules/InputForm.tsx
+++ pages/components/molecules/InputForm.tsx
@@ -9,12 +9,14 @@ interface Props {
   isLoading: boolean;
 }
 
-const C: React.FC<Props> = ({ label, buttonLabel, onClickButton, isLoading }) => (
+const C: React.FC<Props> = props => {
+  const { label, buttonLabel, onClickButton, isLoading } = props;
+  return (
   <div>
     <Label size="h6">{label}</Label>
-    <InputText />
+    <InputText type="number" />
     <Button color="primary" onClick={onClickButton} isDisabled={isLoading}>{buttonLabel}</Button>
   </div>
-)
+)}
 
 export default C;
