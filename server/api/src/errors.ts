e30fa9824f6892765fd470f7a71af9efdbf19d1f
katai5plate <katai5plate.sub@gmail.com>
2019-06-05 07:49:13 +0900

    feature: error handling

--- server/api/src/errors.ts
+++ server/api/src/errors.ts
@@ -1,2 +1,10 @@
-export const INVALID_ID: string = "INVALID_ID";
-export const NOT_FOUND: string = "NOT_FOUND";
+import { Response } from "express";
+
+export enum ErrorCode {
+  INVALID_ID = "INVALID_ID",
+  NOT_FOUND = "NOT_FOUND",
+}
+export const errorHandling = (response: Response, code: ErrorCode, status: number = 500) => {
+  response.status(status);
+  response.send({ error: code });
+}
--- server/api/src/services/getUser.ts
+++ server/api/src/services/getUser.ts
@@ -1,23 +1,14 @@
 import express from "express";
-import * as ERROR from "../errors";
+import { ErrorCode, errorHandling } from "../errors";
 
 import { Client } from "pg";
 import * as PSQL_CONFIG from "../../psql.config";
 
-// const sleepTime = (ms: number) => new Promise(res => setTimeout(res, ms));
-
-// export const wait = (app: express.Express, uri: string): void => {
-//   app.get(uri, async (_, response) => {
-//     await sleepTime(3000);
-//     response.json({ res: "OKEY" })
-//   })
-// }
-
 export default (app: express.Express, uri: string): void => {
-  app.get(uri, async (request, response) => {
+  app.get(uri, async (request, response: express.Response) => {
     const userid = Number(request.params.userid);
     if (!Number.isInteger(userid) || userid === 0) {
-      response.json({ error: ERROR.INVALID_ID })
+      errorHandling(response, ErrorCode.INVALID_ID, 500);
       return;
     }
 
@@ -27,8 +18,7 @@ export default (app: express.Express, uri: string): void => {
     await client.end();
 
     if (rows.length === 0) {
-      response.status(500);
-      response.send({ error: ERROR.NOT_FOUND });
+      errorHandling(response, ErrorCode.NOT_FOUND, 500);
       return;
     }
     response.json({ res: rows[0] })
